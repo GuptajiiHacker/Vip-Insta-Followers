570f1785d207c20cd07b93792906e4a9d2911680
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-01-26 13:02:02 +0530

    Vip-Login.sh

--- Vip-Login.sh
+++ Vip-Login.sh
@@ -1,7 +1,7 @@
 cat banner.txt | lolcat -p .6
 
 
-echo -e "\e[0;33m This trick consist of following & Unfollowing instegrams id"
+echo -e "\e[0;33m This trick consist of following & Unfollowing celegrams id"
 echo -e "made by insta.me/its_Vip_Krishna_Gupta \e[0m"
     			 
 
