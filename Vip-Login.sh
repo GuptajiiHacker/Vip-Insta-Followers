6e6c475cf6ffc20f711c8150914fe4cd9a36e1e2
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-01-26 10:43:03 +0530

    Vip-Login.sh

--- Vip-Login.sh
+++ Vip-Login.sh
@@ -2,7 +2,7 @@ cat banner.txt | lolcat -p .6
 
 
 echo -e "\e[0;33m This trick consist of following & Unfollowing celegrams id"
-echo -e "special thanks to t.me/nirvan_marg \e[0m"
+echo -e "special thanks to t.me/nirvan_mar \e[0m"
     			 
 
 			
