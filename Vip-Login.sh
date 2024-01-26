be8e0f4d6b4f49be575694488a0819fa099dbd1e
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-01-26 12:45:22 +0530

    Vip-Login.sh

--- Vip-Login.sh
+++ Vip-Login.sh
@@ -1,16 +1,11 @@
-
- banner()"
- _  _____       _____ ____  _     _     ____  _      _____ ____  ____ 
-/ \/  __/      /    //  _ \/ \   / \   /  _ \/ \  /|/  __//  __\/ ___\
-| || |  ______ |  __\| / \|| |   | |   | / \|| |  |||  \  |  \/||    \
-| || |_//\____\| |   | \_/|| |_/\| |_/\| \_/|| |/\|||  /_ |    /\___ |
-\_/\____\      \_/   \____/\____/\____/\____/\_/  \|\____\\_/\_\\____/
-
-banner()                                                                                                                                                                                                                                                                                                                                                                              
-
-						      
 cat banner.txt | lolcat -p .6
-    						
+
+
+echo -e "\e[0;33m This trick consist of following & Unfollowing celegrams id"
+echo -e "special thanks to t.me/nirvan_marg \e[0m"
+    			 
+
+			
 read -p "Enter Username : " U
 		         	
 read -p "Enter password :" -s S
